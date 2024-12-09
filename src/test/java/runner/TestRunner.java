e0abe7f13cb03b714e48fdf1b522b1407132806b
Pradeep Kumar <pradeepkr2198@gmail.com>
2024-12-09 11:51:37 +0530

    conflict resolved

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,10 +9,12 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="src/test/java/features",glue= {"stepDefinition","helper"},tags="",plugin="")
 public class TestRunner {
 
+
 //test case  comment for  conflict
 	//comment for git conflict
 
 		//add comment 
 	//add test automation
+
 
 }
