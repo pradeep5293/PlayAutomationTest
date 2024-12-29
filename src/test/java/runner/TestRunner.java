e407025ff611c9c04b3ebf5806d89e59a1ef35c3
Pradeep Kumar <141697774+pradeep5293@users.noreply.github.com>
2024-12-29 12:16:10 +0530

    Update TestRunner.java
    tag added

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",glue= {"stepDefinition","helper"},tags="",
+@CucumberOptions(features="src/test/java/features",glue= {"stepDefinition","helper"},tags="@Reg",
 plugin="json:target/jsonReports/test.json")
 public class TestRunner {
 
